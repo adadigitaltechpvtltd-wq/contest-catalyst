c2a6d8481ff4bf6728d69c27d9db3c0139dc939f
adadigitaltechpvtltd-8414 <adadigitaltechpvtltd@gmail.com>
2025-12-31 23:41:56 +0530

    email change

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gaal.app',
+  appName: 'gaal',
+  webDir: 'dist'
+};
+
+export default config;
